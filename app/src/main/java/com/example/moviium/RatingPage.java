e1664fea2d144088dc1da2b7af45d5752f1064fc
Vithu <vithursan04@gmail.com>
2023-03-10 13:47:12 -0500

    ratingpage intents

--- app/src/main/java/com/example/moviium/RatingPage.java
+++ app/src/main/java/com/example/moviium/RatingPage.java
@@ -2,13 +2,46 @@ package com.example.moviium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class RatingPage extends AppCompatActivity {
+
+    ImageButton btnHome, btnFav, btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating_page);
+
+        btnHome = findViewById(R.id.imgBtnHomeRP);
+        btnFav = findViewById(R.id.imgBtnStarRP);
+        btnProfile = findViewById(R.id.imgBtnProfileRP);
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MovieList.class);
+                startActivity(intent);
+            }
+        });
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Profile.class);
+                startActivity(intent);
+            }
+        });
     }
 }
