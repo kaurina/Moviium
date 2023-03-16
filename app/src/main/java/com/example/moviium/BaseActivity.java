77460c930f30e7dfff11a31cffaf7699c96837f5
Mandeep Lota <mkaur.hunter@gmail.com>
2023-03-16 11:16:17 -0400

    added logout feature

--- app/src/main/java/com/example/moviium/BaseActivity.java
+++ app/src/main/java/com/example/moviium/BaseActivity.java
@@ -1,6 +1,9 @@
 package com.example.moviium;
 
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +11,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class BaseActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    ImageButton logout;
+    public FirebaseAuth mAuth;
 
 
     @Override
@@ -22,5 +26,16 @@ public class BaseActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+
+        logout = findViewById(R.id.imgBtnLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(BaseActivity.this, login.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/moviium/HomePage.java
+++ app/src/main/java/com/example/moviium/HomePage.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomePage extends BaseActivity {
 
-    ImageButton btnHome, btnStar, btnProfile;
+    ImageButton btnHome, btnStar, btnProfile, btnSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,7 @@ public class HomePage extends BaseActivity {
         btnHome = findViewById(R.id.imgBtnHomeHM);
         btnStar = findViewById(R.id.imgBtnStarHM);
         btnProfile = findViewById(R.id.imgBtnProfileHM);
+        btnSignOut = findViewById(R.id.imgBtnLogout);
 
         btnStar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +37,16 @@ public class HomePage extends BaseActivity {
             public void onClick(View v) {
                 Intent intentToProfile = new Intent(getApplicationContext(), Profile.class);
                 startActivity(intentToProfile);
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.getCurrentUser();
+                mAuth.signOut();
+                //Toast.makeText(HomePage.this, "", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(HomePage.this, login.class));
             }
         });
 
